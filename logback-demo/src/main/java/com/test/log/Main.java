4f2f3599ab5164eefbdd90344ab639bce8c9d6f2
wuqr <wql789@qq.com>
2021-03-26 22:03:38 +0800

    增加日志demo配置

---
+++ logback-demo/src/main/java/com/test/log/Main.java
@@ -0,0 +1,14 @@
+package com.test.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    // logback中 这里的Logger类 并不是logback专属的Logger类，而是slf4j的Logger。
+    // logback默认是实现了slf4j标准的
+    private static final Logger LOG= LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.info("logback demo");
+    }
+}
