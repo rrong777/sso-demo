a2f98a4334900d3064f6fc373cd9db49b79cdee7
wuqr <wql789@qq.com>
2021-03-26 23:48:08 +0800

    project 依赖了ABC三个项目，ABC三个项目分别使用了log4j, log4j2, logback

--- log-demo/src/main/java/com/Main.java
+++ log-demo/src/main/java/com/Main.java
@@ -2,6 +2,10 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        com.Main
+        // 如果三个项目分别是使用了log4j，log4j2，logback的实现
+        // 不起冲突 此时没有使用任何配置文件，使用的就是三个被引入的本地项目的配置文件
+        com.rrong777.log4jdemo.Main.main(args);
+        com.rrong777.log4j2demo.Main.main(args);
+        com.rrong777.logbackdemo.Main.main(args);
     }
 }
