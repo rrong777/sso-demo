b63cfcfc72e085dcc44169f6a6adfbf76c470c0d
wuqr <wql789@qq.com>
2021-03-26 22:11:28 +0800

    log4j2实现slf4j标准

--- log4j-demo2/src/main/java/com/Main.java
+++ log4j-demo2/src/main/java/com/Main.java
@@ -1,13 +1,13 @@
 package com;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 //    private static final ;
     // log4j 和 log4j2中的Logger都是自己专属的Logger类。没有实现slf4j的标准
-    private final static Logger LOG = LogManager.getLogger(Main.class);
+    private final static Logger LOG = LoggerFactory.getLogger(Main.class);
 
 
     public static void main(String[] args) {
